a5b4727a65786ab34b8b24b725fa55131d351df6
Venu gopi <mvgopi@lustralwater.com>
2023-05-31 11:17:47 +0000

    creating subscription query modified

--- pages/UserOnboarding/jsobjects/validDate/validDate.js
+++ pages/UserOnboarding/jsobjects/validDate/validDate.js
@@ -5,5 +5,18 @@ export default {
 			const start_Date_format = new Date(subs_start_input.formattedDate);
 		let subs_end_date = new Date(31536000000 + start_Date_format.getTime());
 		return subs_end_date.toISOString();
-	}
+	},
+	nextYear :()=>{
+// Print the result
+var currentDate = new Date(subs_start_input.formattedDate);
+// Add one year
+currentDate.setFullYear(currentDate.getFullYear() + 1);
+		
+return currentDate.toISOString();
+	},
+	selected_Date :()=>{
+		var date = new Date(subs_start_input.formattedDate);
+		date.setDate(date.getDate() + 1)
+		return date
+	},
 }
